0431450c09a99da75e872017adc713059b8725af
Suwadith <suwadith06@gmail.com>
2017-02-18 08:30:35 +0530

    Update

--- src/CodeSprint/Breaking_the_Records.java
+++ src/CodeSprint/Breaking_the_Records.java
@@ -15,26 +15,11 @@ public class Breaking_the_Records {
         for(int score_i=0; score_i < n; score_i++){
             score[score_i] = in.nextInt();
         }
-        // your code goes here
 
-        /*ArrayList<Integer> increase = new ArrayList<>();
-        ArrayList<Integer> decrease = new ArrayList<>();
-        increase.add(score[0]);
-
-        for(int x=1; x<n-1; x++){
-            if(score[x]>score[x+1]){
-                increase.add(x);
-            }
-        }
-
-        System.out.println(increase.size()+""+decrease.size());*/
-
-        int highest = 0;
         int numHighest = 0;
-        int lowest = 0;
         int numLowest = 0;
 
-        highest = score[0];
+        int highest = score[0];
         for(int x=0; x<n; x++){
             if(score[x]>highest){
                 highest = score[x];
@@ -42,7 +27,7 @@ public class Breaking_the_Records {
             }
         }
 
-        lowest = score[0];
+        int lowest = score[0];
         for(int x=0; x<n; x++){
             if(score[x]<lowest){
                 lowest=score[x];
--- src/CodeSprint/Separate_the_Numbers.java
+++ src/CodeSprint/Separate_the_Numbers.java
@@ -1,9 +1,42 @@
 package CodeSprint;
 
+import java.util.Scanner;
+
 /**
  * Created by Suwadith on 2/18/2017.
  */
 public class Separate_the_Numbers {
 
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int q = in.nextInt();
+        for(int a0 = 0; a0 < q; a0++){
+            String s = in.next();
+            /*for(int x=0; x<s.length()-1; x++){
+                for(int y=1; y<=s.length(); y++){
+                    System.out.println(s.substring(x,y));
+                    int num = Integer.parseInt(s.substring(x,y));
+                }
+                break;
+            }*/
+            /*int count1 =0;
+            int[] arr = new int[s.length()];
+            for(int x=0; x<s.length(); x++){
+                int num = Integer.parseInt(s.substring(x,x+1));
+                arr[x]=num;
+
+            }
+            for(int x=0; x<arr.length-1; x++){
+
+                if(arr[x]==(arr[x+1])-1){
+                    count1++;
+                }
+            }
+            if(count1==arr.length-1){
+                System.out.println("YES " + s.charAt(0));
+            }*/
+
+        }
+    }
 
 }
