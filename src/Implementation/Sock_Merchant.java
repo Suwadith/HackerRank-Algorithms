436b7ae494757c173c99068915c03e34a0746ce8
Suwadith <suwadith06@gmail.com>
2017-10-02 11:23:56 +0530

    Update

---
+++ src/Implementation/Sock_Merchant.java
@@ -0,0 +1,42 @@
+package Implementation;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Sock_Merchant {
+
+    static int sockMerchant(int n, int[] ar) {
+        HashSet<Integer> diffColors = new HashSet<>();
+
+        for(int i=0; i<n; i++){
+            diffColors.add(ar[i]);
+        }
+
+        int count = 0;
+        int finalCount = 0;
+        for(int i : diffColors){
+           count = 0;
+            for(int x=0; x<n; x++) {
+                if (i == ar[x]){
+                    count++;
+                }
+            }
+            finalCount += count/2;
+//            System.out.println(count/2);
+        }
+
+        return finalCount;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] ar = new int[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            ar[ar_i] = in.nextInt();
+        }
+        int result = sockMerchant(n, ar);
+        System.out.println(result);
+    }
+
+}
